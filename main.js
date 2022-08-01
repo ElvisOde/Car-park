d9a478620b0c97e5377f4d6d812e8ba3fc2c8947
ElvisOde <97256867+ElvisOde@users.noreply.github.com>
2022-08-01 13:20:30 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,61 @@
+//Create a reference for canvas 
+Canvas = document.getElementById ('myCanvas');
+ctx =Canvas.getContext ("2nd");
+//Give specific height and width to the car image
+Greencar_width = 75;
+greencar_hight = 100;
+background_image = "parkingLot.jpg";
+greencar_image = "car2.png";
+//Set initial position for a car image.
+Greencar_x = 5;
+Greencar_y =
+function add() {
+	//upload car, and background images on the canvas.
+    w
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+		if(keyPressed == '38')
+		{
+			up();
+			console.log("up");
+		}
+	
+		if(keyPressed == '40')
+		{
+			down();
+			console.log("down");
+		}
+		
+		if(keyPressed == '37')
+	wwsd	{
+			left();
+			console.log("left");
+		}
+	
+		if(keyPressed == '39')
+		{
+			right();
+			console.log("right");
+		}
+		
+		
+}
+
+function up()
+{
+if greencar_y =greebcar_y
+
+function down()
+{
+	//Define function to move the car downward
+}
+
+function left()
+{
+	//Define function to move the car left side
+}
+
+function right()
+{
+	//Define function to move the car right side
+}
